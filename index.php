3768d0119a538885a6753f2bb993d7d7417ac2c1
owen-mettam <owentmettam@gmail.com>
2017-06-25 20:00:31 -0400

    index.php
    Main page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>InformationDataDisplay</title>
+    <link rel="stylesheet" type="text/css" href="...">
+  </head>
+  
+  <body>
+    <h1>Just some information</h1>
+    
